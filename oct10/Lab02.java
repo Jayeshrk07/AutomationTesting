54474988b7522ff2040a1251e5d92257fde37dcc
Jayesh Kapadane <jk71197@gmail.com>
2024-10-10 06:59:22 +0530

    All files are added

---
+++ oct10/Lab02.java
@@ -0,0 +1,17 @@
+package Oct.oct10;
+
+public class Lab02 {
+    public static void main(String[] args) {
+    int Enum = 9;
+    System.out.println(Enum);
+
+    int integer = 10;
+    System.out.println(integer);
+
+    int String = 10;
+    System.out.println(String);
+
+   // int enum = 9; // invalid
+    // All keywords in small alphabates
+    }
+}
---
+++ oct10/Lab03_Literals.java
@@ -0,0 +1,9 @@
+package Oct.oct10;
+
+public class Lab03_Literals {
+    public static void main(String[] args) {
+        //literals->
+        //data_type variable_name = variable_value
+
+    }
+}
